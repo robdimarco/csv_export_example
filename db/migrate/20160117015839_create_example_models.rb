1d5e56363244fb8e06b1d6aac1b6e22cdd112585
Rob Di Marco <rob.dimarco@416software.com>
2016-01-16 21:33:42 -0500

    Add more rows

--- db/migrate/20160117015839_create_example_models.rb
+++ db/migrate/20160117015839_create_example_models.rb
@@ -2,7 +2,7 @@ class CreateExampleModels < ActiveRecord::Migration
   def up
     execute <<-SQL
     CREATE VIEW example_models AS
-    select generate_series id, round(random() * 100) sales_count from generate_series(1, 100000);
+    select generate_series id, round(random() * 100) sales_count from generate_series(1, 1000000);
     SQL
   end
 
