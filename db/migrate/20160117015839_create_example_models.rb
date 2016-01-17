dae02f2f3618b6c38c00a66380bb59cd7f4906f1
Rob Di Marco <rob.dimarco@416software.com>
2016-01-16 21:10:23 -0500

    Added example model with 100,000 rows

---
+++ db/migrate/20160117015839_create_example_models.rb
@@ -0,0 +1,14 @@
+class CreateExampleModels < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+    CREATE VIEW example_models AS
+    select generate_series id, round(random() * 100) sales_count from generate_series(1, 100000);
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+    DROP VIEW example_models
+    SQL
+  end
+end
